d40896490d7fcfdbfc3de72dda298d7b113269bf
javaNoviceProgrammer <javaNoviceProgrammer@git.com>
2021-07-25 07:22:04 -0400

    minor update to TimeStamp class

--- movie-timeline/src/main/java/movie_timeline/TimeStamp.java
+++ movie-timeline/src/main/java/movie_timeline/TimeStamp.java
@@ -4,10 +4,22 @@ public class TimeStamp {
 	
 	int timeSeconds ; // total seconds: example: 300 seconds
 	int hours, minutes, seconds ; // timestamp format: hh:mm:ss
+	String fmt ; // extra format for the toString() method
+	
+	public TimeStamp(int timeSeconds, String fmt) {
+		this.timeSeconds = timeSeconds ; // prevent shadowing
+		this.fmt = fmt ;
+		doConversion() ;
+	}
 	
 	public TimeStamp(int timeSeconds) {
 		this.timeSeconds = timeSeconds ; // prevent shadowing
 		doConversion() ;
+	}
+	
+	public TimeStamp setFmt(String fmt) {
+		this.fmt = fmt ;
+		return this ;
 	}
 	
 	private void doConversion() {
@@ -18,7 +30,18 @@ public class TimeStamp {
 
 	@Override
 	public String toString() {
-		return String.format("%s:%s:%s", Utils.toDoubleDigit(hours), Utils.toDoubleDigit(minutes), Utils.toDoubleDigit(seconds)) ;
+		String str = String.format("%s:%s:%s", Utils.toDoubleDigit(hours), Utils.toDoubleDigit(minutes), Utils.toDoubleDigit(seconds)) ;
+		if(fmt == null) {
+			return str ;
+		}
+		else {
+			return String.format(fmt, str) ;
+		}
+	}
+	
+	public static void main(String[] args) {
+		TimeStamp timeStamp = new TimeStamp(1000) ;
+		System.out.println(timeStamp);
 	}
 	
 }
