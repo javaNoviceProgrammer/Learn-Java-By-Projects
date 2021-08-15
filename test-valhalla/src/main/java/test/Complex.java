e9e3f0f46514f19c1b9ed9d11623016ac475e550
javaNoviceProgrammer <javaNoviceProgrammer@git.com>
2021-08-15 13:32:26 -0400

    minor updates.

--- test-valhalla/src/main/java/test/Complex.java
+++ test-valhalla/src/main/java/test/Complex.java
@@ -1,13 +1,17 @@
 package test;
 
 @__inline__
-public class Complex { // value-type classes
+public class Complex { // value-type classes 
 
 	double re, im ;
 	
 	public Complex(double re, double im) {
 		this.re = re ;
 		this.im = im ;
+	}
+	
+	public Complex add(Complex z) {
+		return new Complex(re+z.re, im+z.im) ;
 	}
 
 	@Override
--- test-valhalla/src/main/java/test/Main.java
+++ test-valhalla/src/main/java/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		long startTime = System.currentTimeMillis() ;
 		Complex z1 = new Complex(1.1, 2.2) ;
 		Complex z2 = new Complex(-1.1, -2.2) ;
 		Complex[] w = {z1, z2} ; // inside java heap: flat layout
@@ -15,7 +16,16 @@ public class Main {
 		System.out.println(w[0].getClass());
 		// some changes
 		// code like an object, works like a primitive
+		Complex[] w2 = new Complex[1000_000] ;
+		for(int i=0; i<w2.length; i++) {
+			w2[i] = new Complex(10.0*i, 25.0/i) ;
+		}
 		
+		for(int i=0; i<w2.length; i++) {
+			w2[i] = w2[i].add(z1).add(z2) ;
+		}
+		long endTime = System.currentTimeMillis() ;
+		System.out.println(String.format("total time (msec) = %d", endTime-startTime));
 	}
 
 }
