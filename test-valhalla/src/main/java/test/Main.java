2f2d81e752168378ebe78d047e570519e631adf2
javaNoviceProgrammer <javaNoviceProgrammer@git.com>
2021-08-15 11:47:39 -0400

    minor updates.

--- test-valhalla/src/main/java/test/Main.java
+++ test-valhalla/src/main/java/test/Main.java
@@ -1,5 +1,6 @@
 package test;
 
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -14,7 +15,7 @@ public class Main {
 		System.out.println(w[0].getClass());
 		// some changes
 		// code like an object, works like a primitive
-
+		
 	}
 
 }
